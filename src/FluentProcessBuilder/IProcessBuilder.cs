6262a006396ab376473e07720f6dfccff40631ae
Georgi Barnev <gbarnev@gmail.com>
2023-05-03 00:32:08 +0300

    Generate initial structure of generator

---
+++ src/FluentProcessBuilder/IProcessBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentProcessBuilder
+{
+    public interface IProcessBuilder
+    {
+    }
+}
---
+++ src/Playground/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Playground
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            HelloFrom("Generated code");
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
+
